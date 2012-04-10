2c434001ba2d3638982f95a071fe30c045260f29
Max Gonzih <gonzih@gmail.com>
2012-04-10 16:14:58 +0300

    add heroku configuration

---
+++ heroku_config.sh
@@ -0,0 +1,8 @@
+heroku config:add HUBOT_IRC_SERVER="irc.freenode.net"
+heroku config:add HUBOT_IRC_ROOMS="#shelr"
+heroku config:add HUBOT_IRC_NICK="ShelrHubot"
+heroku config:add HUBOT_IRC_PORT=6697
+#heroku config:add HUBOT_IRC_NICKSERV_PASSWORD=""
+heroku config:add HUBOT_IRC_SERVER_FAKE_SSL="true"
+heroku config:add HUBOT_IRC_UNFLOOD="true"
+heroku config:add HUBOT_IRC_USESSL="true"
