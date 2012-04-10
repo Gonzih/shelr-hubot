6a9a153b9ecc856aaa314b03c7c0c30f639a16cb
Max Gonzih <gonzih@gmail.com>
2012-04-10 16:39:45 +0300

    rename bot to RedPanda

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-app: bin/hubot -a irc -n ShelrHubot
+app: bin/hubot -a irc -n RedPanda
--- heroku_config.sh
+++ heroku_config.sh
@@ -1,6 +1,6 @@
 heroku config:add HUBOT_IRC_SERVER="irc.freenode.net"
 heroku config:add HUBOT_IRC_ROOMS="#shelr"
-heroku config:add HUBOT_IRC_NICK="ShelrHubot"
+heroku config:add HUBOT_IRC_NICK="RedPanda"
 heroku config:add HUBOT_IRC_PORT=6697
 #heroku config:add HUBOT_IRC_NICKSERV_PASSWORD=""
 heroku config:add HUBOT_IRC_SERVER_FAKE_SSL="true"
